e5211403a8ca00a4bd3e83b70849d9bdc9c0825e
vadimZ <01vadim10@gmail.com>
2016-05-17 13:00:57 +0300

    1st commit

---
+++ WpfAppAllCode/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfAppAllCode
+{
+    class Program : Application
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+            app.Startup += AppStartUp;
+            app.Exit += AppExit;
+            app.Run();
+        }
+
+        static void AppExit(object sender, ExitEventArgs e)
+        {
+            MessageBox.Show("App has exited");
+        }
+
+        static void AppStartUp(object sender, StartupEventArgs e)
+        {
+            Window mainWindow = new Window();
+            mainWindow.Title = "My First WPF App!";
+            mainWindow.Height = 200;
+            mainWindow.Width = 300;
+            mainWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            mainWindow.Show();
+        }
+    }
+}
